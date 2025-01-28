173ff19855741df29a0fc94fe2ed0714fdec7dea
Snake54310 <86499062+Snake54310@users.noreply.github.com>
2025-01-28 14:30:05 -0700

    got code to point where it should work, got address send working

--- app/main.asm
+++ app/main.asm
@@ -20,15 +20,15 @@ StopWDT     mov.w   #WDTPW+WDTHOLD,&WDTCTL  ; Stop WDT
 ; (port and pin numbers are placeholders pins not typically enabled for I2C module use)
 ; setting pins as output:
 ; bis.w   #LOCKLPM5,&PM5CTL0 
-; bis.b   #00001100b, &P1DIR  ; choose 3.2 as clock and 3.3 as data
-; bis.b   #00001100b, &P1OUT  (passively high)
+; bis.b   #00001100b, &P3DIR  ; choose 3.3 as clock and 3.2 as data
+; bis.b   #00001100b, &P3OUT  (passively high)
 ; bic.w   #LOCKLPM5,&PM5CTL0 
 
 ;setting pins as input:
 ; bis.w   #LOCKLPM5,&PM5CTL0 
 ; bic.b   #00001100b, &P3DIR
 ; bis.b   #00001100b, &P3REN
-; bic.b   #00001100b, &P1OUT  (pull down resistors, for now)
+; bic.b   #00001100b, &P3OUT  (pull down resistors, for now)
 ; bic.w   #LOCKLPM5,&PM5CTL0 
 
 
@@ -53,12 +53,12 @@ init:
             bic.b   #001000b, &P2OUT  ; (pull down resistors, for now)
 
 
-            bic.b   #000010b, &P4DIR ; switch 1
-            bis.b   #000010b, &P4REN
-            bis.b   #000010b, &P4OUT  ; (pull down resistors, for now)
-            bis.b   #000010b, &P4IES
-            bic.b   #000010b, &P4IFG
-            bis.b   #000010b, &P4IE
+            bic.b   #00000010b, &P4DIR ; switch 1
+            bis.b   #00000010b, &P4REN
+            bis.b   #00000010b, &P4OUT  ; (pull down resistors, for now)
+            bis.b   #00000010b, &P4IES
+            bic.b   #00000010b, &P4IFG
+            bis.b   #00000010b, &P4IE
 
             bis.w   #TBCLR, &TB1CTL         ; clock_clock on control register TB1CTL
             bis.w   #TBSSEL__SMCLK, &TB1CTL  ; choose SMCLK
@@ -73,19 +73,20 @@ init:
             NOP
 
             ; Select I2C pins 
-            bis.b   #00001100b, &P1DIR  ; choose 3.2 as clock and 3.3 as data, outputs as default
-            bis.b   #00001100b, &P1OUT  ; (passively high)
+            bis.b   #00001100b, &P3DIR  ; choose 3.2 as clock and 3.3 as data, outputs as default
+            bis.b   #00001100b, &P3OUT  ; (passively high)
 
             bic.w   #LOCKLPM5,&PM5CTL0       ; Unlock I/O pins
             
             mov.b   #68, R6     ; address of Real Time Clock (I think)
             mov.b   #2, R11     ; default value of status register
             mov.b   #0, R10     ; set perform_send operation to 0
+            bis.b    #BIT2, P3OUT       ; set clock pin to high
+            bis.b    #BIT3, P3OUT       ; set data pin to high
+            NOP
 
 main:
             NOP
-            bis.b    #BIT2, P3OUT       ; set clock pin to high
-            bis.b    #BIT3, P3OUT       ; set clock pin to high
             cmp.b    #1, R10
             jl main
             jge Send_data
@@ -122,9 +123,9 @@ wait:
 Send_data_start:
             mov.b    R6, R7
             mov.b    #3, R11
-            mov.b    #8, R9     ; send 7 bits of data
+            mov.b    #9, R9     ; send 7 bits of data (number should be 8 for this)
             rla.b    R7 ; BSL R7
-            mov.b   #1, R10     ; set perform_send operation to 1
+            bis.b   #00000001b, R10     ; set perform_send operation to 1
             jmp Clear_SW1_Flag
 Clear_SW1_Flag:
             bic.b    #BIT1, P4IFG
@@ -149,7 +150,10 @@ Send_0:
            mov.b    #3, R11      ; wait to send next bit
            jmp Send_data
 End_address_send: 
-            mov.w   #2, R11     ; default value of send-data status register (we will not ever send a larger number than 68)
+            mov.w    #2, R11     ; default value of send-data status register (we will not ever send a larger number than 68)
+            mov.w    #0, R10
+            bis.b    #BIT2, P3OUT       ; set clock pin to high
+            bis.b    #BIT3, P3OUT       ; set data pin to high
             jmp main
 
 ; ------ end Switch1_ISR ----------
@@ -157,6 +161,7 @@ End_address_send:
 ISR_Clock: 
             cmp.b   #3, R11  ; is data being sent (is send status register (R11) empty (value greater than or equal to ~1))
             jge     Switch_Clock    ; if yes, then jump to switch_clock
+            bis.b    #BIT2, P3OUT   ; set clock high as default
         ; also check if value is less than 2-- this will symbolize some type of send operation
 Clear_Clock_Flag:
             bic.w   #CCIFG, &TB1CCTL0  ; clear interrupt flag
@@ -164,7 +169,9 @@ Clear_Clock_Flag:
 
 Switch_Clock:
             xor.b    #BIT2, P3OUT   ; toggle clock output
-            jz Send_Next_Bit; if bit was toggled to low, toggle "send next bit" bit to 1 (jump to new subroutine to perform this operation, =>
+            xor.b   #00000010b, R10     ; set perform_send operation to 1
+            cmp.b   #2, R10             ; compare R10, bit 1 of 0 means r10 is low
+            jge Send_Next_Bit; if bit was toggled to low, toggle "send next bit" bit to 1 (jump to new subroutine to perform this operation, =>
             jmp Clear_Clock_Flag
                 ; => then jump to Clear_clock_flag)
 Send_Next_Bit:
